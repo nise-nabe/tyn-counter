9857931d87badd07dcdc3eea86c70af7eb393af8
nise_nabe <nise.nabe@gmail.com>
2015-02-11 01:46:06 +0900

    輝きが二倍じゃなくて一段階増加という修正

--- app/app.js
+++ app/app.js
@@ -18,49 +18,56 @@ angular.module('myApp', [
             return resource().list().$promise;
         }
     };
+}]).factory('History', [function() {
+    return {
+        getList: function() {
+            var histories = localStorage.getItem("histories");
+            if (histories === null) {
+                histories = [];
+            } else {
+                histories = JSON.parse(histories);
+            }
 
-}]).controller("Controller", ['Resource', '$moment', function(Resource, $moment) {
-    $moment.locale(["ja"]);
-    function getHistories() {
-        var histories = localStorage.getItem("histories");
-        if (histories === null) {
-            histories = [];
-        } else {
-            histories = JSON.parse(histories);
+            return histories;
+        },
+        setList: function(histories) {
+            localStorage.setItem("histories", JSON.stringify(histories));
+        },
+        getListForName: function(name) {
+            return _.filter(this.getList(), function(h) {return h.name === name; });
         }
-        histories = _.map(histories, function(history) {
+
+    }
+}]).controller("Controller", ['Resource', 'History', '$moment', function(Resource, History, $moment) {
+        $moment.locale(["ja"]);
+
+        Resource.list().then(_.bind(function(json) {
+            this.items = _.map(json.items, function(item) {
+                item.counts = _.size(History.getListForName(item.name));
+                item.points = _.reduce(History.getListForName(item.name), function(memo, h) { return memo + h.point; }, 0);
+                item.point = json.type[item.type].point;
+                item.bonus = json.type[Math.min(parseInt(item.type) + 1, 6)].point;
+                return item;
+            });
+            this.type = json.type;
+
+            this.counts = _.reduce(this.items, function(memo, item) {return memo + item.counts}, 0);
+            this.points = _.reduce(this.items, function(memo, item) {return memo + item.points}, 0);
+        }, this), function(error) {
+            console.log(error);
+        });
+
+        this.histories = _.map(_.first(History.getList(), 11), function(history) {
             history.createdAt = $moment(history.created_at).format('LLL');
             return history;
         });
 
-        return histories;
-    }
-    function setHistories(histories) {
-        localStorage.setItem("histories", JSON.stringify(histories));
-    }
-
-    Resource.list().then(_.bind(function(json) {
-        this.items = _.map(json.items, function(item) {
-            var histories = getHistories();
-            item.counts = _.size(_.filter(histories, function(h) {return h.name === item.name; }));
-            item.points = _.reduce(_.filter(histories, function(h) {return h.name === item.name; }),
-                function(memo, h) { return memo + h.point; }, 0);
-            return item;
-        });
-        this.counts = _.reduce(this.items, function(memo, item) {return memo + item.counts}, 0);
-        this.points = _.reduce(this.items, function(memo, item) {return memo + item.points}, 0);
-    }, this), function(error) {
-        console.log(error);
-    });
-
-    this.histories = _.first(getHistories(), 11);
-
-    this.doRecord = function(name, point) {
-        var histories = getHistories();
-        var history = {name: name, point: point, created_at: Date.now()};
-        histories.unshift(history);
-        setHistories(histories);
-        history.createdAt = $moment(history.created_at).format('LLL');
-        this.histories.unshift(history);
-    }
+        this.doRecord = function(name, point) {
+            var histories = History.getList();
+            var history = {name: name, point: point, created_at: Date.now()};
+            histories.unshift(history);
+            History.setList(histories);
+            history.createdAt = $moment(history.created_at).format('LLL');
+            this.histories.unshift(history);
+        }
 }]);
