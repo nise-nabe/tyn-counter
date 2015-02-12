7199a748967779db1c56c48f4d1d5d2f54c98eae
nise_nabe <nise.nabe@gmail.com>
2015-02-13 03:18:10 +0900

    format

--- app/app.js
+++ app/app.js
@@ -39,42 +39,43 @@ angular.module('myApp', [
 
     }
 }]).controller("Controller", ['Resource', 'History', '$moment', function(Resource, History, $moment) {
-        $moment.locale(["ja"]);
+    $moment.locale(["ja"]);
 
-        Resource.list().then(_.bind(function(json) {
-            this.items = _.map(json.items, function(item) {
-                item.counts = _.size(History.getListForName(item.name));
-                item.points = _.reduce(History.getListForName(item.name), function(memo, h) { return memo + h.point; }, 0);
-                item.point = json.type[item.type].point;
-                item.bonus = json.type[Math.min(parseInt(item.type) + 1, 6)].point;
-                return item;
-            });
-            this.type = json.type;
-
-            this.counts = _.reduce(this.items, function(memo, item) {return memo + item.counts}, 0);
-            this.points = _.reduce(this.items, function(memo, item) {return memo + item.points}, 0);
-        }, this), function(error) {
-            console.log(error);
+    Resource.list().then(_.bind(function(json) {
+        this.items = _.map(json.items, function(item) {
+            item.counts = _.size(History.getListForName(item.name));
+            item.points = _.reduce(History.getListForName(item.name), function(memo, h) { return memo + h.point; }, 0);
+            item.point = json.type[item.type].point;
+            item.bonus = json.type[Math.min(parseInt(item.type) + 1, 6)].point;
+            return item;
         });
+        this.type = json.type;
 
-        this.histories = _.map(_.first(History.getList(), 11), function(history) {
-            history.createdAt = $moment(history.created_at).format('LLL');
-            return history;
-        });
+        this.counts = _.reduce(this.items, function(memo, item) {return memo + item.counts}, 0);
+        this.points = _.reduce(this.items, function(memo, item) {return memo + item.points}, 0);
+    }, this), function(error) {
+        console.log(error);
+    });
 
-        this.doRecord = function(name, point) {
-            var histories = History.getList();
-            var history = {name: name, point: point, created_at: Date.now()};
-            histories.unshift(history);
-            History.setList(histories);
-            history.createdAt = $moment(history.created_at).format('LLL');
-            this.histories.unshift(history);
-        };
-        this.doUpdate = _.bind(function(item, point) {
-            item.counts += 1;
-            this.counts += 1;
-            item.points += point;
-            this.points += point;
-        }, this);
+    this.histories = _.map(_.first(History.getList(), 11), function(history) {
+        history.createdAt = $moment(history.created_at).format('LLL');
+        return history;
+    });
+
+    this.doRecord = function(name, point) {
+        var histories = History.getList();
+        var history = {name: name, point: point, created_at: Date.now()};
+        histories.unshift(history);
+        History.setList(histories);
+        history.createdAt = $moment(history.created_at).format('LLL');
+        this.histories.unshift(history);
+    };
+
+    this.doUpdate = _.bind(function(item, point) {
+        item.counts += 1;
+        this.counts += 1;
+        item.points += point;
+        this.points += point;
+    }, this);
 
 }]);
