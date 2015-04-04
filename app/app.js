47f65da7e5922830951787781ac2dd8c485b7d1e
nise_nabe <nise.nabe@gmail.com>
2015-04-05 01:30:37 +0900

    controller 側で保存・取得先を決定するように変更

--- app/app.js
+++ app/app.js
@@ -21,8 +21,8 @@ angular.module('myApp', [
     };
 }]).factory('History', [function() {
     return {
-        getList: function() {
-            var histories = localStorage.getItem("histories");
+        getList: function(target) {
+            var histories = localStorage.getItem(target);
             if (histories === null) {
                 histories = [];
             } else {
@@ -31,21 +31,22 @@ angular.module('myApp', [
 
             return histories;
         },
-        setList: function(histories) {
-            localStorage.setItem("histories", JSON.stringify(histories));
-        },
-        getListForName: function(name) {
-            return _.filter(this.getList(), function(h) {return h.name === name; });
+        setList: function(target, histories) {
+            localStorage.setItem(target, JSON.stringify(histories));
         }
-
     }
 }]).controller("Controller", ['Resource', 'History', '$moment', function(Resource, History, $moment) {
     $moment.locale(["ja"]);
 
     Resource.list().then(_.bind(function(json) {
+        var histories =  History.getList("histories");
         this.items = _.map(json.items, function(item) {
-            item.counts = _.size(History.getListForName(item.name));
-            item.points = _.reduce(History.getListForName(item.name), function(memo, h) { return memo + h.point; }, 0);
+            var listForName = function(list, name) {
+                return _.filter(list, function(h) {return h.name === name; });
+            }(histories, item.name);
+
+            item.counts = _.size(listForName);
+            item.points = _.reduce(listForName, function(memo, h) { return memo + h.point; }, 0);
             item.point = json.type[item.type].point;
             item.bonus = json.type[Math.min(parseInt(item.type) + 1, 6)].point;
             return item;
@@ -58,28 +59,28 @@ angular.module('myApp', [
         console.log(error);
     });
 
-    this.histories = _.map(_.first(History.getList(), 11), function(history) {
+    this.histories = _.map(_.first(History.getList('histories'), 11), function(history) {
         history.createdAt = $moment(history.created_at).format('LLL');
         return history;
     });
 
     this.doRecord = function(name, point) {
-        var histories = History.getList();
+        var histories = History.getList('histories');
         var history = {name: name, point: point, created_at: Date.now()};
         histories.unshift(history);
-        History.setList(histories);
+        History.setList("histories", histories);
         history.createdAt = $moment(history.created_at).format('LLL');
         this.histories.unshift(history);
     };
 
     this.deleteRecord = _.bind(function(history) {
-        var histories = History.getList();
+        var histories = History.getList('histories');
         for(var i = 0; i < histories.length; ++i) {
             var h = histories[i];
             if (h.name === history.name && h.point == history.point && h.created_at == history.created_at) {
                 histories.splice(i, 1);
                 this.histories.splice(i, 1);
-                History.setList(histories);
+                History.setList('histories', histories);
 
                 this.counts -= 1;
                 this.points -= h.point;
